ee08597a9362c439868526b6b234417d28b0939d
Matteo De Micheli <mat.demicheli@gmail.com>
2013-12-11 22:19:51 +0100

    First commit

---
+++ leaderboard.js
@@ -0,0 +1,90 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+
+if (Meteor.isClient) { 
+
+  Template.leaderboard.players = function () {
+    var selector = {},
+        searchResults = Session.get('ids');
+
+    if ("undefined" !== typeof searchResults && searchResults.length > 0) {
+      selector = { '_id' : { '$in' : searchResults } };
+    }
+
+    return Players.find(selector, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    },
+    'keyup #search' : function (e) {
+        EasySearch.search('players', $(e.target).val(), function (error, data) {
+            data = _(data).map(function (doc) {
+                return doc._id;
+            });
+
+            Session.set('ids', data);
+        });
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  });
+}
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var first_names = ["Ada",
+           "Grace",
+           "Marie",
+           "Carl",
+           "Nikola",
+           "Claude",
+           "Peter",
+           "Stefan",
+           "Stephen",
+           "Lisa"],
+           last_names = ["Lovelace",
+           "Hopper",
+           "Curie",
+           "Tesla",
+           "Shannon",
+           "Müller",
+           "Meier",
+           "Miller",
+           "Gaga",
+           "Franklin"];
+      for (var i = 0; i < 30; i++)
+        Players.insert({
+            name: (first_names[Math.floor(Math.random() * 10)] + ' ' + last_names[Math.floor(Math.random() * 10)]), 
+            score: Math.floor(Random.fraction()*10)*5});
+    }
+  });
+}
+
+// Searching
+Meteor.startup(function () {
+    // on Client and Server
+    EasySearch.createSearchIndex('players', {
+      'collection'    : Players,              // instanceof Meteor.Collection
+      'field'         : ['name', 'score'],    // can also be an array of fields
+      'limit'         : 20                   // default: 10
+    });
+});
